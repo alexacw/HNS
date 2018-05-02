b0f6ebb5433071021bb137edaad01b919d245541
alexacw <alex_acw@outlook.com>
2018-05-02 14:37:07 +0800

    .

--- embedded/.dep/crt1.o.d
+++ embedded/.dep/crt1.o.d
@@ -1,24 +1,22 @@
 build/obj/crt1.o: \
  /home/acw/Desktop/HNS/embedded/os/common/startup/ARMCMx/compilers/GCC/crt1.c \
- /usr/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
  /home/acw/Desktop/HNS/embedded/os/common/startup/ARMCMx/devices/STM32F1xx/cmparams.h \
  /home/acw/Desktop/HNS/embedded/board/board.h \
  /home/acw/Desktop/HNS/embedded/os/common/ext/ST/STM32F1xx/stm32f1xx.h \
  /home/acw/Desktop/HNS/embedded/os/common/ext/ST/STM32F1xx/stm32f103xb.h \
  /home/acw/Desktop/HNS/embedded/os/common/ext/ARM/CMSIS/Core/Include/core_cm3.h \
- /usr/lib/gcc/arm-none-eabi/7.2.1/include/stdint.h \
- /usr/arm-none-eabi/include/stdint.h \
- /usr/arm-none-eabi/include/machine/_default_types.h \
- /usr/arm-none-eabi/include/sys/features.h \
- /usr/arm-none-eabi/include/_newlib_version.h \
- /usr/arm-none-eabi/include/sys/_intsup.h \
- /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_stdint.h \
  /home/acw/Desktop/HNS/embedded/os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h \
  /home/acw/Desktop/HNS/embedded/os/common/ext/ARM/CMSIS/Core/Include/cmsis_compiler.h \
  /home/acw/Desktop/HNS/embedded/os/common/ext/ARM/CMSIS/Core/Include/cmsis_gcc.h \
  /home/acw/Desktop/HNS/embedded/os/common/ext/ST/STM32F1xx/system_stm32f1xx.h
 
-/usr/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h:
+/usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h:
 
 /home/acw/Desktop/HNS/embedded/os/common/startup/ARMCMx/devices/STM32F1xx/cmparams.h:
 
@@ -30,19 +28,17 @@ build/obj/crt1.o: \
 
 /home/acw/Desktop/HNS/embedded/os/common/ext/ARM/CMSIS/Core/Include/core_cm3.h:
 
-/usr/lib/gcc/arm-none-eabi/7.2.1/include/stdint.h:
+/usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
 
-/usr/arm-none-eabi/include/stdint.h:
+/usr/include/newlib/stdint.h:
 
-/usr/arm-none-eabi/include/machine/_default_types.h:
+/usr/include/newlib/machine/_default_types.h:
 
-/usr/arm-none-eabi/include/sys/features.h:
+/usr/include/newlib/sys/features.h:
 
-/usr/arm-none-eabi/include/_newlib_version.h:
+/usr/include/newlib/sys/_intsup.h:
 
-/usr/arm-none-eabi/include/sys/_intsup.h:
-
-/usr/arm-none-eabi/include/sys/_stdint.h:
+/usr/include/newlib/sys/_stdint.h:
 
 /home/acw/Desktop/HNS/embedded/os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h:
 
