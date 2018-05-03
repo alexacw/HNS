0a66635917af61a6c9032c16482f4de50ed8d69b
alexacw <alex_acw@outlook.com>
2018-05-03 23:32:57 +0800

    .

--- embedded/.dep/vectors.o.d
+++
@@ -1,5 +0,0 @@
-build/obj/vectors.o: \
- /home/acw/Desktop/HNS/embedded/os/common/startup/ARMCMx/compilers/GCC/vectors.S \
- /home/acw/Desktop/HNS/embedded/os/common/startup/ARMCMx/devices/STM32F1xx/cmparams.h
-
-/home/acw/Desktop/HNS/embedded/os/common/startup/ARMCMx/devices/STM32F1xx/cmparams.h:
